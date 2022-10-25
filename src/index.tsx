37338ae2f4b0cd383768290e03a440f1b1fd5a5d
Thomas Lamb <thomaslambprogramming@gmail.com>
2022-10-25 18:40:51 +1100

    Fixed accidental css deletion

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import App from './App';
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
+import './App.css';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  <App />,
-  // </StrictMode>,
+  <StrictMode>
+    <App />,
+  </StrictMode>,
 );
